ea0ae08ad5456e8345f3267160881d7f32936dc0
BuildTools <unconfigured@null.spigotmc.org>
2022-08-10 17:25:47 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import alpha_vantage
+import pyautogui
+import secrets
+
+# Concept:
+# We want to create a stock screener that allows the user to sort through stocks based on criteria such as price and
+# volume and allows the user to set alerts for when their chosen stocks meet certain criteria.
+
+# This will be accomplished using the Alpha Vantage API to get stock information.
+# Currently, we do not know how alerts will be handled. More research is required.
+# The gui part of the program will be handled by pyautogui.
+
---
+++ secrets.py
@@ -0,0 +1,4 @@
+# This file is not meant to be uploaded to GitHub
+# It will hold the API key
+
+api_key = "TDTQISBHKYN77N58"
